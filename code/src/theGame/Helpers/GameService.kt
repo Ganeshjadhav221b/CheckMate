43a2222ec3c831a7537e4578cac2f5ecae6c10a5
Ganesh Jadhav <ganesh.jadhav@hashedin.com>
2020-12-09 12:07:31 +0530

    removed unneeded files

--- code/src/theGame/Helpers/GameService.kt
+++ code/src/theGame/Helpers/GameService.kt
@@ -6,6 +6,29 @@ import theGame.Classes.*
 import theGame.Interfaces.IGame
 import theGame.Interfaces.IGameService
 
+fun initialize()
+{
+//    println(CardSuit.CLUBS)
+//    println(CardValue.Ace)
+//    println(Messages.NewGame)
+        val deck: Deck = Deck()
+        //print(deck.cards)
+//    var card: Card = deck.cards[0]
+//    println("Card is $card")
+//
+//    deck.Shuffle()
+//      card = deck.cards[0]
+//    println("Card $card")
+//    //deck.cards.remove(card)
+//    println("Size ${deck.cards.size}")
+    val player1: Player = Player("ganeshjadhav221b@gmail.com","root1234","Ganesh",100)
+    val dealer1: Player = Player("dealer@acmecasinos.com","root1234","dealer",1000)
+    val table1:Table = Table("Earth",100)
+    val game : Game = Game(player1,dealer1,table1)
+    //println("Drawn: ${game.DrawCard()}")
+    game.Play()
+
+}
 
 class GameService: IGameService
 {
